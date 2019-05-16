0daa5c7f9998950f6de5dafb92482c71e07108e4
Bruce Hill <bhill@slac.stanford.edu>
2019-05-16 00:37:38 -0700

    Modify default rx.cmd for khz tests

--- iocBoot/ioctest/rx.cmd
+++ iocBoot/ioctest/rx.cmd
@@ -8,10 +8,10 @@ var(collectorCaDebug, 1)
 var(collectorDebug, 1)
 var(receiverPVADebug, 1)
 
-var(maxEventRate, 40.0)
-var(collectorCaScalarMaxRate, 20.0)
-var(collectorCaArrayMaxRate, 1.5)
-var(bsasFlushPeriod, 2.0)
+var(maxEventRate, 2000.0)
+var(collectorCaScalarMaxRate, 2000.0)
+var(collectorCaArrayMaxRate, 120)
+var(bsasFlushPeriod, 10.0)
 
 bsasTableAdd("RX:")
 
